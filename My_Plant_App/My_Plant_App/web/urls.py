9925c451145c4d9687b088e6d8b8daced800c52d
GalkaKG <galina_georgieva_net@abv.bg>
2023-05-24 08:36:07 +0300

    added templates and static files

--- My_Plant_App/My_Plant_App/web/urls.py
+++ My_Plant_App/My_Plant_App/web/urls.py
@@ -1,3 +1,26 @@
+from django.urls import path
+
+from My_Plant_App.web.views import index
+
+from . import views
+
 urlpatterns = (
+    path('', views.index, name='home'),
 
 )
+
+
+# TODO:
+'''
+    •	http://localhost:8000/ - home page
+    •	http://localhost:8000/profile/create/ - profile create page
+    •	http://localhost:8000/catalogue/ - catalogue
+    •	http://localhost:8000/create/ - plant create page
+    •	http://localhost:8000/details/<plant_id>/ - plant details page
+    •	http://localhost:8000/edit/<plant_id>/ - plant edit page
+    •	http://localhost:8000/delete/<plant_id>/ - plant delete page
+    •	http://localhost:8000/profile/details/ - profile details page
+    •	http://localhost:8000/profile/edit/ - profile edit page
+    •	http://localhost:8000/profile/delete/ - profile delete page
+
+'''
