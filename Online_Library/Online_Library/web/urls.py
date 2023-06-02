f5bca541346ecb4cd12a0191fe0da0ec5ccefb49
GalkaKG <galina_georgieva_net@abv.bg>
2023-06-02 22:36:53 +0300

    new commit

--- Online_Library/Online_Library/web/urls.py
+++ Online_Library/Online_Library/web/urls.py
@@ -3,4 +3,21 @@ from . import views
 
 urlpatterns = (
     path('', views.index, name='home page'),
+    path('add/', views.add_book, name='add book'),
+
 )
+
+
+# TODO:
+
+'''
+•	http://localhost:8000/ - home page
+
+•	http://localhost:8000/add/ - add book page
+•	http://localhost:8000/edit/:id - edit book page
+•	http://localhost:8000/details/:id - book details page
+
+•	http://localhost:8000/profile/ - profile page
+•	http://localhost:8000/profile/edit/ - edit profile page
+•	http://localhost:8000/profile/delete/ - delete profile page0
+'''
--- Online_Library/Online_Library/web/views.py
+++ Online_Library/Online_Library/web/views.py
@@ -3,3 +3,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/home-with-profile.html')
+
+
+def add_book(request):
+    return render(request, 'book/add-book.html')
+
+
+def edit_book(request):
+    return render(request, 'book/edit-book.html')
+
+
+def book_details(request):
+    return render(request, 'book/book-details.html')
+
+
+def profile(request):
+    return render(request, 'profile/profile.html')
+
+
+def edit_profile(request):
+    return render(request, 'profile/edit-profile.html')
+
+
+def delete_profile(request):
+    return render(request, 'profile/delete-profile.html')
