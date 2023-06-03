c6ff29a64763d5070206c530a41e60fa6de02544
GalkaKG <galina_georgieva_net@abv.bg>
2023-06-03 21:44:21 +0300

    new commit

---
+++ Notes_App/Notes_App/web/urls.py
@@ -0,0 +1,16 @@
+urlpatterns = (
+    
+)
+
+
+#TODO: 
+
+'''
+•	http://localhost:8000/ - home page
+•	http://localhost:8000/add - add note page
+•	http://localhost:8000/edit/:id - edit note page
+•	http://localhost:8000/delete/:id - delete note page
+•	http://localhost:8000/details/:id - note details page
+•	http://localhost:8000/profile - profile page
+
+'''
