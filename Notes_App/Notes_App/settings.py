27041f3e3640c37719e02d1ca77f88ebd0c3d9e2
GalkaKG <galina_georgieva_net@abv.bg>
2023-06-04 19:34:29 +0300

    new commit

--- Notes_App/Notes_App/settings.py
+++ Notes_App/Notes_App/settings.py
@@ -61,13 +61,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Notes_App.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "notes_db",
+        "USER": "postgres-user",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
