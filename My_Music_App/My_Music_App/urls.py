bd00b546e5f41b20f8fdbe0c239c635b0adb0072
GalkaKG <galina_georgieva_net@abv.bg>
2023-06-15 12:59:18 +0300

    new commit

--- My_Music_App/My_Music_App/urls.py
+++ My_Music_App/My_Music_App/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('My_Music_App.web.urls.py'))
+    path('', include('My_Music_App.web.urls'))
 ]
