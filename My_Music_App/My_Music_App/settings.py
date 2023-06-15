ecd9d9d4872936eb8ed35368cf6a243d34c51bfe
GalkaKG <galina_georgieva_net@abv.bg>
2023-06-15 11:25:31 +0300

    new commit

--- My_Music_App/My_Music_App/settings.py
+++ My_Music_App/My_Music_App/settings.py
@@ -67,9 +67,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- My_Music_App/My_Music_App/urls.py
+++ My_Music_App/My_Music_App/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('My_Music_App.web.urls.py'))
 ]
