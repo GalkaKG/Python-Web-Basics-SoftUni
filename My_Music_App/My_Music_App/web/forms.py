c8786c5157273fa4e5e819a61e29b8664d1a6993
GalkaKG <galina_georgieva_net@abv.bg>
2023-06-20 14:07:50 +0300

    new commit

--- My_Music_App/My_Music_App/web/forms.py
+++ My_Music_App/My_Music_App/web/forms.py
@@ -14,4 +14,22 @@ class CreateAlbumForm(forms.ModelForm):
         fields = '__all__'
 
 
+class EditAlbumForm(CreateAlbumForm):
+    pass
+
+
+class DeleteAlbumForm(CreateAlbumForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__set_disabled_fields()
+
+    def save(self, commit=True):
+        if commit:
+            self.instance.delete()
+        return self.instance
+
+    def __set_disabled_fields(self):
+        for field in self.fields.values():
+            field.disabled = True
+
 
--- My_Music_App/My_Music_App/web/views.py
+++ My_Music_App/My_Music_App/web/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from My_Music_App.web.forms import CreateProfileForm, CreateAlbumForm
+from My_Music_App.web.forms import CreateProfileForm, CreateAlbumForm, EditAlbumForm, DeleteAlbumForm
 from My_Music_App.web.models import Profile, Album
 
 
@@ -10,6 +10,10 @@ def get_profile():
 
 def get_all_albums():
     return Album.objects.all()
+
+
+def get_album(pk):
+    return Album.objects.get(pk=pk)
 
 
 def home(request):
@@ -42,10 +46,26 @@ def home(request):
 
 
 def details_profile(request):
-    return render(request, 'profile/profile-details.html')
+    profile = get_profile()
+    albums = get_all_albums()
+
+    context = {
+        'profile': profile,
+        'count_albums': len(albums)
+    }
+
+    return render(request, 'profile/profile-details.html', context)
 
 
 def delete_profile(request):
+    profile = get_profile()
+    albums = get_all_albums()
+
+    if request.method == 'GET':
+        albums.delete()
+        profile.delete()
+        return redirect('home page')
+
     return render(request, 'profile/profile-delete.html')
 
 
@@ -61,16 +81,52 @@ def add_album(request):
     context = {
         'form': form
     }
+
     return render(request, 'album/add-album.html', context)
 
 
 def details_album(request, pk):
-    return render(request, 'album/album-details.html')
+    album = get_album(pk)
+
+    context = {
+        'album': album
+    }
+
+    return render(request, 'album/album-details.html', context)
 
 
 def edit_album(request, pk):
-    return render(request, 'album/edit-album.html')
+    album = get_album(pk)
+
+    if request.method == 'GET':
+        form = EditAlbumForm(instance=album)
+    else:
+        form = EditAlbumForm(request.POST, instance=album)
+        if form.is_valid():
+            form.save()
+            return redirect('home page')
+    context = {
+        'album': album,
+        'form': form
+    }
+
+    return render(request, 'album/edit-album.html', context)
 
 
 def delete_album(request, pk):
-    return render(request, 'album/delete-album.html')
+    album = get_album(pk)
+
+    if request.method == 'GET':
+        form = DeleteAlbumForm(instance=album)
+    else:
+        form = DeleteAlbumForm(request.POST, instance=album)
+        if form.is_valid():
+            form.save()
+            return redirect('home page')
+
+    context = {
+        'album': album,
+        'form': form
+    }
+
+    return render(request, 'album/delete-album.html', context)
