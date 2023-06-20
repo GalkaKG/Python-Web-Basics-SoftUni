3da5c14563bd6f249239d3839a74737330006e88
GalkaKG <galina_georgieva_net@abv.bg>
2023-06-20 18:25:52 +0300

    new commit

--- My_Music_App/My_Music_App/web/forms.py
+++ My_Music_App/My_Music_App/web/forms.py
@@ -6,6 +6,23 @@ class CreateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = '__all__'
+        widgets = {
+            'username': forms.TextInput(
+                attrs={
+                    'placeholder': 'Username'
+                }
+            ),
+            'email': forms.EmailInput(
+                attrs={
+                    'placeholder': 'Email'
+                }
+            ),
+            'age': forms.NumberInput(
+                attrs={
+                    'placeholder': 'Age'
+                }
+            ),
+        }
 
 
 class CreateAlbumForm(forms.ModelForm):
