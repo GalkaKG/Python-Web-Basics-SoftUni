ab94cea060e0ae317e89dc10f4f58dddf0e441b5
GalkaKG <galina_georgieva_net@abv.bg>
2023-06-20 22:17:28 +0300

    new commit

--- Expenses_Tracker/Expenses_Tracker/settings.py
+++ Expenses_Tracker/Expenses_Tracker/settings.py
@@ -1,22 +1,13 @@
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-49sjdrn&kk)1*d^$)h!tm7+!zj&-ip+1%045=zy!j+7^mqncln'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -72,10 +63,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -92,9 +79,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -104,13 +88,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (BASE_DIR / 'static/',)
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- Expenses_Tracker/Expenses_Tracker/web/models.py
+++ Expenses_Tracker/Expenses_Tracker/web/models.py
@@ -1,10 +1,63 @@
+from django.core import validators
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
-class Profile(models.Model):
-    pass
+def validate_name(value):
+    if not value.isalpha():
+        raise ValidationError('Ensure this value contains only letters.')
 
+
+def validate_image_size(value):
+    max_size = value * 1024 * 1024
+    if value > max_size:
+        raise ValidationError('Max file size is 5.00 MB')
+
+
+class Profile(models.Model):
+    first_name = models.CharField(
+        max_length=15,
+        validators=(
+            validators.MinLengthValidator(2),
+            validate_name,
+        )
+    )
+
+    last_name = models.CharField(
+        max_length=15,
+        validators=(
+            validators.MinLengthValidator(2),
+            validate_name,
+        )
+    )
+
+    budget = models.FloatField(
+        default=0,
+        validators=(
+            validators.MinValueValidator(0),
+        )
+    )
+
+    profile_image = models.ImageField(
+        blank=True,
+        null=True,
+        default='/static/images/user.png',
+        validators=(
+            validate_image_size,
+        ),
+    )
 
 
 class Expense(models.Model):
-    pass
+    title = models.CharField(
+        max_length=30,
+    )
+
+    expense_image = models.URLField()
+
+    description = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    price = models.FloatField()
